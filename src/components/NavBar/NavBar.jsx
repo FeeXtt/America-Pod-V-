899e60298829fe015cf4b2a584fd808e3d98340e
Michalovronka <kokosnahlave123seznam.cz>
2024-05-30 19:57:36 +0200

    Navbar Update

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,7 +3,9 @@ import AmericaLogo from "./AmericaLogo.png";
 import PodVeziLogo from "./PodVeziLogo.png";
 import "./NavBar.css";
 
-const navItems = ['ÚVOD', 'MENU', 'GALERIE', 'KONTAKT'];
+import { Link } from 'react-router-dom';
+
+const navItems = [['ÚVOD', "/"], ['MENU', "/menu"], ['GALERIE', "/galerie"], ['KONTAKT', "/kontakt"]];
 
 export default function NavBar() {
   const [scrollY, setScrollY] = useState(0);
@@ -27,10 +29,9 @@ export default function NavBar() {
   const [isNavOpen, setIsNavOpen] = useState(false);
   
   return (
-    <nav className="font-notable bg-red-900 text-white sticky top-0">
+    <nav className="font-notable bg-red-900 text-white sticky top-0 z-50">
       <div className="md:flex items-center justify-between max-w-screen-xl mx-auto p-6">
 
-        {/* <div className='flex invisible md:visible'> */}
           <div className="fixed bg-red-900 invisible md:visible" style={logoStyle}>
             <img src={AmericaLogo} alt="America Logo" className="max-h-60 " />
             <img src={PodVeziLogo} alt="America Logo" className="max-w-60 -mt-1 mb-2" />
@@ -38,7 +39,7 @@ export default function NavBar() {
 
           <ul className="flex space-x-8 ml-auto invisible md:visible">
             {navItems.map((item, index) => (
-              <li key={index} className="text-lg ">{item}</li>
+              <li key={index} className="text-lg hover:text-orange-100"><Link to={item[1]}>{item[0]}</Link></li>
             ))}
           </ul>
         {/* </div> */}
@@ -46,27 +47,26 @@ export default function NavBar() {
         <div className='md:hidden visible'>
           
           <div className='flex'>
-            {/* <div>
-              <img src={AmericaLogo} alt="America Logo" className="max-w-20 " />
-            </div> */}
             <div >
               <img src={PodVeziLogo} alt="America Logo" className="fixed max-w-60 -mt-11" />
             </div>
 
             <div
-              className="space-y-2 ml-auto -mt-6"
+              className="space-y-2 ml-auto -mt-6 cursor-pointer *:hover:bg-orange-100"
               onClick={() => setIsNavOpen((prev) => !prev)}
             >
-              <span className="block h-0.5 w-8 bg-white"></span>
-              <span className="block h-0.5 w-8 bg-white"></span>
-              <span className="block h-0.5 w-8 bg-white"></span>
+
+              <span className="block h-0.5 w-8 bg-white "></span>
+              <span className="block h-0.5 w-8 bg-white "></span>
+              <span className="block h-0.5 w-8 bg-white "></span>
+
             </div>
           </div>
 
           <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
             <div
-              className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
-              onClick={() => setIsNavOpen(false)} // change isNavOpen state to false to close the menu
+              className="CROSS-ICON absolute top-0 right-0 m-8 cursor-pointer hover:text-orange-100"
+              onClick={() => setIsNavOpen(false)}
             >
               <svg
                 className="h-8 w-8 "
@@ -80,19 +80,16 @@ export default function NavBar() {
                 <line x1="18" y1="6" x2="6" y2="18" />
                 <line x1="6" y1="6" x2="18" y2="18" />
               </svg>
+
             </div>
 
-            <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between bg-red-900 ">
-              <li className="border-b border-gray-400 my-8 uppercase">
-                <a href="/about">About</a>
-              </li>
-              <li className="border-b border-gray-400 my-8 uppercase">
-                <a href="/portfolio">Portfolio</a>
-              </li>
-              <li className="border-b border-gray-400 my-8 uppercase">
-                <a href="/contact">Contact</a>
-              </li>
-            </ul>
+
+            <ul className="MENU-MOBILE-OPEN flex flex-col items-center justify-between ">
+            {navItems.map((item, index) => (
+                <li key={index} className="my-10 text-3xl hover:text-orange-100 border-b pb-1 border-white hover:border-orange-100"><Link to={item[1]}>{item[0]}</Link></li>
+              ))}
+          </ul>
+
           </div>
         </div>
         
