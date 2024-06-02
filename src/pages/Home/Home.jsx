a4a0ffa5b4b55de9c55df4e48527f7a0a798139f
Stanislav Cinkl <stanislav.cinkl@student.smspmb.cz>
2024-06-02 19:58:09 +0200

    trochu mazanicka

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,9 @@
 import NavBar from "../../components/NavBar/NavBar";
 import Footer from "../../components/Footer/Footer";
 import ImageCard from "../../components/ImageCard/ImageCard";
-import InfoCard from "../../components/InfoCard/InfoCard";
+
 import BugerImg from "./burger.png"
-import HomeInfoLayout from "../../components/HomeInfoLayout/HomeInfoLayout";
+
 
 import AboutUsCard from "../../components/AboutUsCard/AboutUsCard";
 import Carousel from "../../components/Carousel/Carousel";
@@ -16,9 +16,6 @@ export default function Home() {
     <maincontent className = "flex flex-col min-h-screen">
       <NavBar />
       <ImageCard src={BugerImg} textpt1={"Vítejte na stránkách restaurace"} textpt2 ={"Amerika Pod Věží"}/>
-      <div className="max-w-screen-xl mx-auto">
-        <HomeInfoLayout />
-      </div>
       <div className="max-w-screen-xl mx-auto ">
         <AboutUsCard/>
       </div>
