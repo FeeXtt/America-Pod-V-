3f2b6024527986cb18dfe291f57f93677a886788
Michal Dvořák <kokosnahlave123@seznam.cz>
2024-06-10 18:40:26 +0200

    OfficialniGalerie

--- src/pages/Galerie/Galerie.jsx
+++ src/pages/Galerie/Galerie.jsx
@@ -1,82 +1,150 @@
-import React from "react";
-import NavBar from "../../components/NavBar/NavBar";
+import { useState } from "react";
 import Footer from "../../components/Footer/Footer";
+import ImageCard from "../../components/ImageCard/ImageCard";
+import NavBar from "../../components/NavBar/NavBar";
+import "flowbite";
+import BugerImg from "../Home/burger.png";
+import photosData from "./photos.json";
+
+const navMenus = [
+  ["Interiér", "Interiér"],
+  ["Jídla", "Jídla"],
+  ["Akce", "Akce"],
+];
 
 export default function Galerie() {
-  return (
-    <>
-      <maincontent className="flex flex-col min-h-screen ">
-        <NavBar />
-        <div className="max-w-screen-xl mx-auto font-mate bg-black">
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta
-            aliquid doloribus error provident. Esse omnis ab neque ullam veniam
-            sapiente delectus officia voluptas atque quae earum facilis quis,
-            ducimus obcaecati.
-          </p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta
-            aliquid doloribus error provident. Esse omnis ab neque ullam veniam
-            sapiente delectus officia voluptas atque quae earum facilis quis,
-            ducimus obcaecati.
-          </p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta
-            aliquid doloribus error provident. Esse omnis ab neque ullam veniam
-            sapiente delectus officia voluptas atque quae earum facilis quis,
-            ducimus obcaecati.
-          </p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta
-            aliquid doloribus error provident. Esse omnis ab neque ullam veniam
-            sapiente delectus officia voluptas atque quae earum facilis quis,
-            ducimus obcaecati.
-          </p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta
-            aliquid doloribus error provident. Esse omnis ab neque ullam veniam
-            sapiente delectus officia voluptas atque quae earum facilis quis,
-            ducimus obcaecati.
-          </p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta
-            aliquid doloribus error provident. Esse omnis ab neque ullam veniam
-            sapiente delectus officia voluptas atque quae earum facilis quis,
-            ducimus obcaecati.
-          </p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta
-            aliquid doloribus error provident. Esse omnis ab neque ullam veniam
-            sapiente delectus officia voluptas atque quae earum facilis quis,
-            ducimus obcaecati. Lorem ipsum dolor sit, amet consectetur
-            adipisicing elit. Soluta aliquid doloribus error provident. Esse
-            omnis ab neque ullam veniam sapiente delectus officia voluptas atque
-            quae earum facilis quis, ducimus obcaecati. Lorem ipsum dolor sit,
-            amet consectetur adipisicing elit. Soluta aliquid doloribus error
-            provident. Esse omnis ab neque ullam veniam sapiente delectus
-            officia voluptas atque quae earum facilis quis, ducimus obcaecati.
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta
-            aliquid doloribus error provident. Esse omnis ab neque ullam veniam
-            sapiente delectus officia voluptas atque quae earum facilis quis,
-            ducimus obcaecati. Lorem ipsum dolor sit, amet consectetur
-            adipisicing elit. Soluta aliquid doloribus error provident. Esse
-            omnis ab neque ullam veniam sapiente delectus officia voluptas atque
-            quae earum facilis quis, ducimus obcaecati. Lorem ipsum dolor sit,
-            amet consectetur adipisicing elit. Soluta aliquid doloribus error
-            provident. Esse omnis ab neque ullam veniam sapiente delectus
-            officia voluptas atque quae earum facilis quis, ducimus obcaecati.
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta
-            aliquid doloribus error provident. Esse omnis ab neque ullam veniam
-            sapiente delectus officia voluptas atque quae earum facilis quis,
-            ducimus obcaecati. Lorem ipsum dolor sit, amet consectetur
-            adipisicing elit. Soluta aliquid doloribus error provident. Esse
-            omnis ab neque ullam veniam sapiente delectus officia voluptas atque
-            quae earum facilis quis, ducimus obcaecati.
-          </p>
-        </div>
+  const [activeTab, setActiveTab] = useState(navMenus[0][1]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalImage, setModalImage] = useState("");
+  const [modalCaption, setModalCaption] = useState("");
 
-        <Footer />
-      </maincontent>
-    </>
+  const openModal = (src, alt) => {
+    setModalImage(src);
+    setModalCaption(alt);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <div className="text-white">
+      <NavBar />
+
+      <ImageCard
+        src={BugerImg}
+        textpt1="Galerie"
+        h="h-80"
+        border={"border-b-2 border-white"}
+      />
+
+      <div className="bg-slate-800 ">
+        <ul
+          className="flex justify-center -mb-px font-notable"
+          id="default-tab"
+          data-tabs-toggle="#default-tab-content"
+          role="tablist"
+        >
+          {navMenus.map((item, index) => (
+            <li key={index} className="text-lg me-2" role="presentation">
+              <button
+                className={`inline-block p-4 text-lg transform hover:drop-shadow-slate900 hover:-translate-y-0.25 hover:-translate-x-0.25 text-white ${
+                  activeTab === item[1]
+                    ? "transform drop-shadow-slate900 -translate-y-0.25 -translate-x-0.25"
+                    : ""
+                }`}
+                onClick={() => setActiveTab(item[1])}
+                id={item[1] + "-tab"}
+                data-tabs-target={"#" + item[1]}
+                type="button"
+                role="tab"
+                aria-controls={item[1]}
+                aria-selected={activeTab === item[1]}
+              >
+                {item[0]}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <br />
+
+      <div id="default-tab-content flex justify-center">
+        {navMenus.map((item, index) => (
+          <div
+            key={index}
+            className={`max-w-screen-lg mx-auto ${
+              activeTab === item[1] ? "" : "hidden"
+            }`}
+            id={item[1]}
+            role="tabpanel"
+            aria-labelledby={`${item[0]}-tab`}
+          ></div>
+        ))}
+      </div>
+
+      <div className=" w-2/3 mx-auto">
+        {" "}
+        {/* ratio na fotky 3:4 pls*/}
+        <div className=" grid xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 col-span-full gap-4 row-end-auto ">
+          {Object.keys(photosData).map((items) => {
+            if (items === activeTab) {
+              return photosData[items].map((photo, i) => {
+                return (
+                  <img
+                    key={i}
+                    src={`${photo.path}`}
+                    alt={`Photo ${photo.id}`}
+                    className="hover:cursor-pointer hover:opacity-70"
+                    onClick={() => openModal(photo.path, `Photo ${photo.id}`)}
+                  />
+                );
+              });
+            }
+          })}
+        </div>
+      </div>
+
+      {modalOpen && (
+        <div className="fixed z-10 inset-0 mt-9">
+          <div className="flex items-center justify-center min-h-screen ">
+            <div className="fixed inset-0 bg-black opacity-75"></div>
+            <div className="relative">
+              <div
+                className="m-2 CROSS-ICON hover:cursor-pointer absolute top-0  right-0 font-bold inline-block rounded-lg bg-red-900 uppercase leading-normal text-white
+                transition duration-150 ease-in-out active:bg-red-800 transform hover:drop-shadow-slate800 hover:-translate-y-0.5 hover:-translate-x-0.5" 
+                onClick={closeModal}
+              >
+                <svg
+                  className="h-8 w-8 hover:text-orange-100"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <line x1="18" y1="6" x2="6" y2="18" />
+                  <line x1="6" y1="6" x2="18" y2="18" />
+                </svg>
+              </div>
+              <img
+                src={modalImage}
+                alt={modalCaption}
+                className="w-full h-256 rounded-lg"
+              />
+              <div className="text-center mt-4">
+
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      <br />
+      <Footer />
+    </div>
   );
 }
