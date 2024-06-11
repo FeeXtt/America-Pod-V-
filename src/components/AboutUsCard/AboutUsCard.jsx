33ff5234a0a701ba6f99001f04c8639be90e282d
Stanislav Cinkl <stanislav.cinkl@student.smspmb.cz>
2024-06-11 18:34:17 +0200

    main hotova

--- src/components/AboutUsCard/AboutUsCard.jsx
+++ src/components/AboutUsCard/AboutUsCard.jsx
@@ -5,15 +5,15 @@ export default function AboutUsCard(props) {
   return (
     <>
       <>
-        <section className="grid gap-8 md:grid-cols-2 md:items-center md:text-left text-center sm:mt-20 mt-20 mb-20 ">
-          <div className={`relative md:px-0 sm:mx-20 md:mx-0 hidden sm:block`}>
+        <section className="grid gap-8 md:grid-cols-2 md:items-center md:text-left text-center  sm:mb-20 sm:mt-20 mt-14 mb-14 ">
+          <div className={`relative md:px-0 sm:mx-20 md:mx-0 mx-5 hidden sm:block`}>
             <img
-              className={`object-cover sm:rounded-lg w-full aspect-[4/3]`}
+              className={`object-cover rounded-lg w-full aspect-[4/3]`}
               src={props.imgSrc}
               alt="burger img"
             />
           </div>
-          <div className="md:px-0 font-mate lg:text-2xl text-xl md:mx-0 sm:mx-20 ">
+          <div className="md:px-0 mx-5 font-mate lg:text-2xl text-xl md:mx-0 sm:mx-20 ">
             <p className="lg:text-6xl text-5xl">
               <b>{props.headline}</b>
             </p>
